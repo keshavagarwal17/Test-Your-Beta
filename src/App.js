ad9f44e122e09005337b36c4077896c53b7916cb
nlok5923 <nlokhande5923@gmail.com>
2021-12-10 19:43:41 +0530

    currently only few elements are in dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from "./Component/Shared/Header/Header";
 import Home from './Component/Home/Home';
+import Dashboard from './Component/Dashboard/Dashboard';
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -12,6 +13,7 @@ const App = () => {
           <Header />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/dashboard" component={Dashboard} />
           </Switch>
           </Router>
     </div>
