abaace095b0133779d4da6cf61ab14dcd4118065
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-12-10 21:42:23 +0530

    added google authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from "./Component/Shared/Header/Header";
+import UserProvider from './providers/userProvider';
 import Home from './Component/Home/Home';
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
@@ -8,12 +9,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   return (
     <div>
-          <Router>
+      <UserProvider>
+        <Router>
           <Header />
           <Switch>
             <Route exact path="/" component={Home} />
           </Switch>
-          </Router>
+        </Router>
+      </UserProvider>
     </div>
   );
 }
