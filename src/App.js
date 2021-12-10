4a50c319e97a91661eb79827735d01e0cfceb992
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-12-11 00:12:30 +0530

    added option to connect with celo wallet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import "./App.css";
 import Header from "./Component/Shared/Header/Header";
-import UserProvider from './providers/userProvider';
-import Home from './Component/Home/Home';
-import Dashboard from './Component/Dashboard/Dashboard';
+import UserProvider from "./providers/userProvider";
+import Home from "./Component/Home/Home";
+import Dashboard from "./Component/Dashboard/Dashboard";
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
-import Create from './Component/Dashboard/Create/Create';
-import ProductPage from './Component/ProductPage/ProductPage';
+import Create from "./Component/Dashboard/Create/Create";
+import ProductPage from "./Component/ProductPage/ProductPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ContractKitProvider } from "@celo-tools/use-contractkit";
-import "@celo-tools/use-contractkit/lib/styles.css";
+import Web3 from "web3";
+import { newKitFromWeb3 } from "@celo/contractkit";
+const connectCeloWallet = async () => {
+  if (window.celo) {
+    try {
+      await window.celo.enable();
+      const web3 = new Web3(window.celo);
+      let kit = newKitFromWeb3(web3);
 
+      const accounts = await kit.web3.eth.getAccounts();
+      const user_address = accounts[0];
+
+      kit.defaultAccount = user_address;
+    } catch (error) {
+      console.log("There is an error");
+      console.log({ error });
+    }
+  } else {
+    console.log("please install the extension");
+  }
+};
 const App = () => {
   return (
     <div>
-      <ContractKitProvider>
       <UserProvider>
         <Router>
           <Header />
@@ -26,9 +43,9 @@ const App = () => {
           </Switch>
         </Router>
       </UserProvider>
-      </ContractKitProvider>
-      </div>
+    </div>
   );
 };
 
 export default App;
+export { connectCeloWallet };
--- src/Component/Shared/Header/Header.jsx
+++ src/Component/Shared/Header/Header.jsx
@@ -1,41 +1,29 @@
 import React, { useContext } from "react";
 import "./Header.scss";
-<<<<<<< HEAD
-import { useContractKit } from "@celo-tools/use-contractkit";
-import { ContractKitProvider } from "@celo-tools/use-contractkit";
-import "@celo-tools/use-contractkit/lib/styles.css";
-=======
-import {signInWithGoogle} from '../../../services/auth'
->>>>>>> 503c19d6d0e3f825a9a3f199a1de844a316541f6
+import { signInWithGoogle } from "../../../services/auth";
 // import { UserContext } from "../../../Provider/UserAddressProvider";
-
+import { connectCeloWallet } from "../../../App";
 const Header = () => {
-  // const info = useContext(UserContext);
-  // const { userAddress } = info;
-  // console.log(userAddress);
 
-  const handleSignIn = async()=>{
+  const handleSignIn = async () => {
     const tem = await signInWithGoogle();
-  }
+  };
 
   return (
     <div className="head">
       <img className="head-logo" src="/asset/images/logo.png" alt="logo" />
       <div className="head-content">
         <div className="head-content-profile">
-<<<<<<< HEAD
-          <h1>Login with google</h1>
-=======
-          <h1 onClick={handleSignIn}>
-            Login with google
-          </h1>
->>>>>>> 503c19d6d0e3f825a9a3f199a1de844a316541f6
+          <h1 onClick={handleSignIn}>Login with google</h1>
         </div>
         <div className="head-content-profile">
           <h1>See Products</h1>
         </div>
         <div className="head-content-profile">
           <h1>About</h1>
+        </div>
+        <div className="head-content-profile">
+          <h1 onClick={connectCeloWallet}>Sign in with Celo Wallet</h1>
         </div>
       </div>
     </div>
