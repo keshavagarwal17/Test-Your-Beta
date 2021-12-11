1dfd8f397e0b635ef1773d78977c6394124409b8
Darshan Hande <darshanhande11@gmail.com>
2021-12-11 16:11:08 +0530

    ui changes for selected role

--- src/Component/PostSignUp/Roles/SelectRole.jsx
+++ src/Component/PostSignUp/Roles/SelectRole.jsx
@@ -1,44 +1,71 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect} from 'react'
+import { Redirect } from 'react-router-dom';
 import {UserContext} from '../../../providers/userProvider'
 import { updateUserInfo } from "../../../services/auth";
 import {
     Segment,
+    Card,
+    Button,
     Form,
-    Button
   } from "semantic-ui-react";
 
 const SelectRole = () => {
     const [role, setRole] = React.useState('');
     const {info,fetchInfo} = useContext(UserContext);
     const {user,isLoading} = info;
-
-    const handleRoleChange = (e, { value }) => setRole(value);
+    const [redirect,setRedirect] = React.useState(null);
 
     const handleSubmit = async()=>{
         await updateUserInfo({role},user.uid);
         fetchInfo();
+        if(role === 'user') {
+            setRedirect('/user-form');
+        } else {
+            setRedirect('/company-form');
+        }
+    }
+
+    const handleRoleChange = (e, { value }) => setRole(value);
+
+    if (redirect) {
+        return <Redirect to={redirect} />
     }
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '3vh', }}>
             <Segment style={{ padding: '20vh 40vw' }}>
                 <h1>Select your Role</h1>
-                <Form.Group inline>
-                    <Form.Radio
-                        label='User'
-                        value='user'
-                        checked={role === 'user'}
-                        onChange={handleRoleChange}
-                        style={{ margin: '2vh auto', fontSize: '1vw'}}
-                    />
-                    <Form.Radio
-                        label='Company'
-                        value='company'
-                        checked={role === 'company'}
-                        onChange={handleRoleChange}
-                        style={{ margin: '2vh auto', fontSize: '1vw'}}
-                    />
-                </Form.Group>
+                <Card.Group>
+                        <Card 
+                            fluid color= 'green' header='User'>
+                            <Card.Content>
+                                <Card.Header>
+                                    <Form.Radio
+                                        label='User'
+                                        value='user'
+                                        checked={role === 'user'}
+                                        onChange={handleRoleChange}
+                                        style={{ margin: '2vh auto', fontSize: '1vw'}}
+                                    />
+                                </Card.Header>
+                            </Card.Content>
+                        </Card>
+                        <Card 
+                            fluid color= 'green' header='Company'>
+                            <Card.Content>
+                                <Card.Header>
+                                    <Form.Radio
+                                        label='Company'
+                                        value='company'
+                                        checked={role === 'company'}
+                                        onChange={handleRoleChange}
+                                        style={{ margin: '2vh auto', fontSize: '1vw'}}
+                                    />
+                                </Card.Header>
+                            </Card.Content>
+                        </Card>
+                </Card.Group>
+
                 <Button
                     color="green"
                     style={{ marginTop: "4vh" }}
@@ -47,6 +74,7 @@ const SelectRole = () => {
                 >
                     Next
                 </Button>
+
             </Segment>
         </div>
     )
